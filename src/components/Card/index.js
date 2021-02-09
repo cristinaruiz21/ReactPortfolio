88b1880d5ba9b2e2456f0164ea51cedc2cc3a559
Cristina Ruiz <cristinaruiz96@yahoo.com>
2021-02-08 18:37:52 -0600

    Changed layout of buttons on portfolio to side by side

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Row from "../Row";
+import Col from "../Col";
 import Button from "../Button";
 import Thought from "../../images/ThoughtBubble.png";
 import Weather from "../../images/Weather1.png";
@@ -16,69 +17,106 @@ function Card() {
       <div className="card">
         <a href="https://secret-reaches-35397.herokuapp.com/" target="_blank" className="link-text">Thought Bubble</a>
         <img src={Thought} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app" id="demo">
           <a href="https://secret-reaches-35397.herokuapp.com/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app" id="git">
           <a href="https://github.com/cristinaruiz21/ThoughtBubble" target="_blank">GitHub</a>
         </Button>
+        </Col>
+        </Row>
       </div>
       <div className="card">
         <a href="https://cristinaruiz21.github.io/WeatherDashboard/" target="_blank" className="link-text">Weather Dashboard</a>
         <img src={Weather} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app">
           <a href="https://cristinaruiz21.github.io/WeatherDashboard/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6">
         <Button className="btn btn-primary btn-lg app">
           <a href="https://github.com/cristinaruiz21/WeatherDashboard" target="_blank">GitHub</a>
         </Button>
+        </Col>
+        </Row>
       </div>
       </Row>
       <Row>
       <div className="card">
         <a href="https://fur-family-34.herokuapp.com/" target="_blank" className="link-text">Fur Family</a>
         <img src={FurFamily} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app">
           <a href="https://fur-family-34.herokuapp.com/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6">
         <Button className="btn btn-primary btn-lg app">
           <a href="https://github.com/cristinaruiz21/FurFamily" target="_blank">GitHub</a>
         </Button>
+        </Col>
+      </Row>
       </div>
       <div className="card">
         <a href="https://summer-mute.github.io/NCBI-Model-Organism-Inspector/" target="_blank" className="link-text">NCBI Model Organism Inpector</a>
         <img src={Organism} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app">
           <a href="https://summer-mute.github.io/NCBI-Model-Organism-Inspector/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6">
         <Button className="btn btn-primary btn-lg app">
           <a href="https://github.com/summer-mute/NCBI-Model-Organism-Inspector" target="_blank">GitHub</a>
         </Button>
+        </Col>
+        </Row>
       </div>
       </Row>
       <Row>
       <div className="card">
         <a href="https://cristinaruiz21.github.io/WorkDayScheduler/" target="_blank" className="link-text">Work Day Scheduler</a>
         <img src={Scheduler} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app">
           <a href="https://cristinaruiz21.github.io/WorkDayScheduler/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6">
         <Button className="btn btn-primary btn-lg app">
           <a href="https://github.com/cristinaruiz21/WorkDayScheduler" target="_blank">GitHub</a>
         </Button>
+        </Col>
+        </Row>
       </div>
       <div className="card">
         <a href="https://note-taker-348.herokuapp.com/" target="_blank" className="link-text">Note Taker</a>
         <img src={NoteTaker} className="img-thumbnail2" alt="password-generator"/>
+        <Row>
+        <Col size="md-6"> 
         <Button className="btn btn-primary btn-lg app">
           <a href="https://note-taker-348.herokuapp.com/" target="_blank">Demo</a>
         </Button>
+        </Col>
+        <Col size="md-6">
         <Button className="btn btn-primary btn-lg app">
           <a href="https://github.com/cristinaruiz21/NoteTaker" target="_blank">GitHub</a>
         </Button>
+        </Col>
+        </Row>
       </div>
       </Row>
     </div>
+    
   );
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,23 +5,29 @@ import Col from "../components/Col";
 
 function Contact() {
   return (
-    <div className="page">
+    <div className="page contact2">
         <Container>
             <h1>Contact Me</h1>
             <Row>
-                <Col size="md-12">
+                <Col size="md-3">
                     <div className="icon LinkedIn">
                         <h3 className="contact">LinkedIn</h3>
                         <a href="https://www.linkedin.com/in/cristina-ruiz-47527a134" rel="noreferrer" target="_blank" className="fab fa-linkedin"></a>
                     </div>
+                </Col>
+                <Col size="md-3">
                     <div className="icon github">
                         <h3 className="contact">GitHub</h3>
                         <a href="https://github.com/cristinaruiz21" rel="noreferrer" target="_blank" className="fab fa-github"></a>
                     </div>
+                </Col>
+                <Col size="md-3">
                     <div className="icon phone">
                         <h3 className="contact">(512)-826-7879</h3>
                         <i className="fas fa-phone"></i>
                     </div>
+                </Col>
+                <Col size="md-3">
                     <div className="icon Resume">
                         <h3 className="contact">Resume</h3>
                         <a href="https://drive.google.com/file/d/17-1yV4E4SKzbmXqKz2vQNW7F29QMNOhw/view?usp=sharing" rel="noreferrer" target="_blank" className="fas fa-file"></a>
